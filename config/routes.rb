a45a844d0d4bb3931a4ce0f2b61d9e478639d641
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-06 10:30:13 +0000

    Devise実装やり直し完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Public（エンドユーザー）用のルーティング
+  # エンドユーザーのページはコントローラをpublicで管理、URLにはPublicをつけずに管理する。
+  scope module: 'public' do
+    devise_for :users, controllers: {
+      registrations: 'public/registrations',
+      sessions: 'public/sessions',
+      passwords: 'public/passwords'
+    }
+    resources :users, only: [:show, :edit, :update]
+    root to: 'homes#top'
+    get '/about' => "homes#about"
+    get '/feed' => "homes#feed"
+  end
+
+  # Admin（管理者）用のルーティング
+  # 管理者のページはコントローラもURLも明示的にadminとわかるように管理する。
+  namespace :admin do
+    devise_for :admins, controllers: {
+      sessions: 'admin/sessions'
+    }, path: ''
+  end
 end
