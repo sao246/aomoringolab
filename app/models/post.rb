87da59de282b0231ecb50c94321fab565a6934f6
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-08 03:14:57 +0000

    モデルのアソシエーション設定追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  # アソシエーション追加 2025/04/08
+  belongs_to :user
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  # アソシエーション追加 2025/04/08
+  has_many :post_tags, dependent: :destroy
+  has_many :posts, through: :post_tags
 end
