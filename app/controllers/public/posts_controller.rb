194a2c9cb3a6e5de70577b031af3df52999fbaa4
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-08 14:04:38 +0000

    投稿機能の土台完成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,23 +1,49 @@
 class Public::PostsController < ApplicationController
   #コントローラ追加 2025/04/07
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: '投稿が完了しました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: '投稿が更新されました。'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path, notice: '投稿が削除されました。'
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
   end
 end
