a9f065fc81d68c704035e2ce12f02fb2d61db835
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-09 02:51:14 +0000

    フラッシュメッセージの挙動不具合修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  # リダイレクト時にフラッシュをクリア
+  after_action :clear_flash, only: [:new, :create, :edit, :update, :destroy]
   protected
   def configure_permitted_parameters
     # ユーザー登録時
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     # ユーザー編集時
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
+  
+  private
+  def clear_flash
+    flash.clear if flash[:alert].present? || flash[:notice].present?
+  end
 end
