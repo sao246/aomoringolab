7b58eb6bbe47f63bb58d234d66c902c2ab08011f
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-14 02:53:20 +0000

    puma設定再修正

--- config/puma.rb
+++ config/puma.rb
@@ -44,9 +44,10 @@ plugin :tmp_restart
 
 # デプロイ準備 2025/04/12
 rails_root = Dir.pwd
-bind "unix://#{rails.root}/tmp/sockets/puma.sock"
+bind "unix://#{rails_root}/tmp/sockets/puma.sock"
 # 本番環境のみデーモン起動
 if ENV['RAILS_ENV'] == 'production'
+  require 'puma/daemon'
   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
