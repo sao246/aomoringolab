fe5065b5ce275e7ae3703bde102661194cde0e25
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-16 07:56:28 +0000

    管理者機能の投稿一覧、コメント機能実装完了

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,12 +1,29 @@
 class Admin::PostsController < ApplicationController
-  #コントローラ追加 2025/04/07
+  # パラメータ設定の処理追加
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # ログインしていない人への処理制御
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
+    #set_post呼び出しするので、個別のPost.findは実施しない。
     @comment = Comment.new
+  end
 
+  def destroy
+    #set_post呼び出しするので、個別のPost.findは実施しない。
+    @post.destroy
+    redirect_to admin_posts_path, notice: '投稿を削除しました。'
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
   end
 end
