ddfce51a86b531dfe8e8fc71c01739df458b9168
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-17 06:51:10 +0000

    adminのキーワード検索実装

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -1,15 +1,22 @@
 class Admin::SearchController < ApplicationController
   #コントローラ追加 2025/04/16
+  # admin権限ログイン者のみ処理可能とする
   before_action :authenticate_admin!
 
   # 検索、検索結果表示用のメソッド追加 2025/04/14
   def index
+    # 検索対象のモデルを指定する
     @model = params[:model]
+    # 検索するキーワードを設定する変数
     @content = params[:content]
+    # 検索方法を指定する（今回は部分一致のみ）
     @method = params[:method]
+
     if @model == 'user'
+    # モデルがUserなら、Userモデルにおいて部分一致でキーワード検索する。
       @records = User.search_for(@content, @method)
     else
+    # モデルがPostなら、Postモデルにおいて部分一致でキーワード検索する。
       @records = Post.search_for(@content, @method)
     end
   end
