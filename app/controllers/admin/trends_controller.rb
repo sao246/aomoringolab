bfd0c8d664453a2948585c1ce299b9f8fd5eb470
“sao246” <195246331+sao246@users.noreply.github.com>
2025-04-27 07:17:44 +0000

    adminのトレンド修正

--- app/controllers/admin/trends_controller.rb
+++ app/controllers/admin/trends_controller.rb
@@ -1,12 +1,12 @@
 class Admin::TrendsController < ApplicationController
   #コントローラ追加 2025/04/18
   def index
-    # いいねされている数が多いユーザー順に並べる
-    @user_likes = User.joins(:favorites)
-                      .where(favorites: { created_at: Time.current.beginning_of_month..Time.current.end_of_month })
-                      .group(:id, :name)
-                      .select('users.id, users.name, COUNT(favorites.id) as favorites_count')
-                      .order('favorites_count DESC')
+    @post_likes = Post
+    .left_joins(:favorites)
+    .where(posts: { created_at: 6.months.ago..Time.current }) # Postのcreated_atで範囲指定
+    .group(:id)
+    .select('posts.id, posts.title, COUNT(favorites.id) as favorites_count')
+    .order('favorites_count DESC')
 
     # タグの累積使用回数を集計して、使用回数の多い順に並べて、上位20件を取得
     @tag_ranking = Tag
